0e3cd0789b7c01c5aa37d7dc08c703a2bd6b27e6
phopps <pathopps1@gmail.com>
2023-05-19 00:09:56 -0400

    added launch line

--- Main.cs
+++ Main.cs
@@ -2,34 +2,66 @@ using Godot;
 
 public partial class Main : Node
 {
-  private Vector2 start;
-  private Vector2 end;
-  private Vector2 impulse;
-  private RigidBody2D player;
-  // private bool isPreparing;
+    private Vector2 start;
+    private Vector2 end;
+    private Vector2 impulse;
+    private RigidBody2D player;
+    private Line2D launchLine;
+    private int startPointIndex = 0;
+    private int endPointIndex = 1;
+    private bool isPreparing = false;
 
-  public override void _Ready()
-  {
-    player = GetNode<RigidBody2D>("Player");
-  }
-
-  public override void _Input(InputEvent @event)
-  {
-    if (@event is InputEventMouseButton eventMouseButton)
+    public override void _Ready()
     {
-      if (eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
-      {
-        start = eventMouseButton.Position;
-        GD.Print("\nStart position = ", eventMouseButton.Position);
-      }
-      else if (eventMouseButton.ButtonIndex == MouseButton.Left)
-      {
-        end = eventMouseButton.Position;
-        GD.Print("End position = ", eventMouseButton.Position);
+        player = GetNode<RigidBody2D>("Player");
+        launchLine = GetNode<Line2D>("LaunchLine");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton eventMouseButton)
+        {
+            if (eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
+            {
+                startLaunchMode(eventMouseButton.Position);
+            }
+            else if (eventMouseButton.ButtonIndex == MouseButton.Left)
+            {
+                endLaunchMode(eventMouseButton.Position);
+            }
+        }
+        else if (@event is InputEventMouseMotion eventMouseMotion && isPreparing)
+        {
+            launchLine.SetPointPosition(endPointIndex, eventMouseMotion.Position);
+        }
+    }
+
+    private void startLaunchMode(Vector2 startPosition)
+    {
+        isPreparing = true;
+        launchLine.Visible = true;
+        start = startPosition;
+        launchLine.SetPointPosition(startPointIndex, start);
+        GD.Print("\nStart position = ", startPosition);
+    }
+
+    private void endLaunchMode(Vector2 endPosition)
+    {
+        end = endPosition;
+        GD.Print("End position = ", endPosition);
         impulse = start - end;
         GD.Print("Launch impulse = ", impulse);
         player.ApplyImpulse(impulse);
-      }
+        isPreparing = false;
+        launchLine.Visible = false;
+        // could animate line to move endpoint toward start point over time duration
     }
-  }
 }
+
+/*
+prep to launch
+  make line visible
+  set start point to start position
+  end point set to current mouse position
+  any mouse movement
+*/
